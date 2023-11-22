4bae985871052f7b3543319355ab2bf2316c4e48
Prithvi <63956587+24-km@users.noreply.github.com>
2023-11-22 10:41:08 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -1,24 +1,13 @@
-from gpiozero import SMBus
+import smbus
+import time
 
-address = 0x08  # Arduino I2C address
-bus = SMBus(1)  # 1 indicates /dev/i2c-1
+# Raspberry Pi's I2C address
+DEVICE_ADDRESS = 0x08
 
-def read_vibration_value():
-    try:
-        value = bus.read_byte(address)
-        return value
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+# Create an I2C bus instance
+bus = smbus.SMBus(1)  # Raspberry Pi 3 uses I2C bus 1
 
-try:
-    while True:
-        vibration_value = read_vibration_value()
-
-        if vibration_value is not None:
-            print(f"Received Vibration Value: {vibration_value}")
-
-        time.sleep(1)  # Adjust the sleep time based on your needs
-
-except KeyboardInterrupt:
-    print("Program terminated by the user.")
+while True:
+    data = bus.read_byte(DEVICE_ADDRESS)
+    print("Vibration Sensor Value:", data)
+    time.sleep(1)  # Adjust delay as needed
