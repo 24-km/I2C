28999f7f7c36583250b291a779faea662ee484c7
Prithvi <63956587+24-km@users.noreply.github.com>
2023-11-22 14:14:07 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -1,7 +1,33 @@
-from smbus import SMBus
+import serial
+import time
 
-addr = 0x8 # bus address
-bus = SMBus(1) # indicates /dev/ic2-1
-bus.write_byte(addr, 0x1) # switch it on
-input("Press return to exit")
-bus.write_byte(addr, 0x0) # switch it on
+# Serial port and baud rate
+ser = serial.Serial('/dev/ttyACM0', 9600)  # Replace '/dev/ttyACM0' with the appropriate port
+
+def receive_data():
+    try:
+        while True:
+            if ser.in_waiting > 0:
+                # Read received data from Arduino via serial
+                received_data = ser.readline().decode('utf-8').rstrip()
+                process_received_data(received_data)
+            
+            time.sleep(0.1)  # Adjust delay as needed
+    except KeyboardInterrupt:
+        pass
+
+def process_received_data(data):
+    # Interpret the received data based on the type of sensor
+    sensor_data = data.split(':')
+    if len(sensor_data) == 2:
+        sensor_type, value = sensor_data[0], sensor_data[1]
+        
+        if sensor_type == 'SPEED':
+            print(f"SPEED: {value}")
+        elif sensor_type == 'Vibration Sensor':
+            print(f"Vibration Sensor: {value}")
+        elif sensor_type == 'Sound Sensor':
+            print(f"Sound Sensor: {value}")
+
+if __name__ == "__main__":
+    receive_data()
