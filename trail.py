94b74a9ae62af13face5cbd399b9f24d6ca08565
Prithvi <63956587+24-km@users.noreply.github.com>
2023-11-22 11:07:19 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -1,13 +1,16 @@
-import smbus
+from smbus import SMBus
 import time
 
-# Raspberry Pi's I2C address
-DEVICE_ADDRESS = 0x08
-
-# Create an I2C bus instance
-bus = smbus.SMBus(1)  # Raspberry Pi 3 uses I2C bus 1
+addr = 0x8  # Arduino's I2C address
+bus = SMBus(1)  # I2C bus number (Raspberry Pi 4 uses bus 1)
 
 while True:
-    data = bus.read_byte(DEVICE_ADDRESS)
-    print("Vibration Sensor Value:", data)
+    bus.write_byte(addr, ord('R'))  # Sending 'R' to request sensor data from Arduino
+    time.sleep(1)  # Give time for Arduino to process and respond
+    
+    # Read a byte from the Arduino (response from the vibration sensor)
+    sensor_data = bus.read_byte(addr)
+    
+    # Print the received sensor data
+    print("Vibration Sensor Value:", sensor_data)
     time.sleep(1)  # Adjust delay as needed
