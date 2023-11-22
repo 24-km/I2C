d3560db19be35e4d5d6084ab4e56d07f50a6e993
Prithvi <63956587+24-km@users.noreply.github.com>
2023-11-22 13:47:51 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -1,25 +1,7 @@
 from smbus import SMBus
-import time
 
-bus = SMBus(1)
-address = 0x08  # Arduino's I2C address
-
-def receive_sensor_data():
-    try:
-        while True:
-            # Read two bytes from Arduino (higher and lower bytes)
-            high_byte = bus.read_byte(address)
-            low_byte = bus.read_byte(address)
-            
-            # Combine two bytes to get the sensor value
-            sensor_value = (high_byte << 8) | low_byte
-            
-            # Print received sensor data
-            print("Received Vibration Sensor Value:", sensor_value)
-            
-            time.sleep(1)  # Adjust delay as needed
-    except KeyboardInterrupt:
-        pass
-
-if __name__ == "__main__":
-    receive_sensor_data()
+addr = 0x8 # bus address
+bus = SMBus(1) # indicates /dev/ic2-1
+bus.write_byte(addr, 0x1) # switch it on
+input("Press return to exit")
+bus.write_byte(addr, 0x0) # switch it on
