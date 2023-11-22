6a3ca12c180142117dd29c517905bbbb9ea3fa93
Prithvi <63956587+24-km@users.noreply.github.com>
2023-11-22 12:26:58 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -1,16 +1,25 @@
+#  Raspberry Pi Master for Arduino Slave
+#  i2c_master_pi.py
+#  Connects to Arduino via I2C
+  
+#  DroneBot Workshop 2019
+#  https://dronebotworkshop.com
+
 from smbus import SMBus
-import time
 
-addr = 0x8  # Arduino's I2C address
-bus = SMBus(1)  # I2C bus number (Raspberry Pi 4 uses bus 1)
+addr = 0x8 # bus address
+bus = SMBus(1) # indicates /dev/ic2-1
 
-while True:
-    bus.write_byte(addr, ord('R'))  # Sending 'R' to request sensor data from Arduino
-    time.sleep(1)  # Give time for Arduino to process and respond
-    
-    # Read a byte from the Arduino (response from the vibration sensor)
-    sensor_data = bus.read_byte(addr)
-    
-    # Print the received sensor data
-    print("Vibration Sensor Value:", sensor_data)
-    time.sleep(1)  # Adjust delay as needed
+numb = 1
+
+print ("Enter 1 for ON or 0 for OFF")
+while numb == 1:
+
+	ledstate = input(">>>>   ")
+
+	if ledstate == "1":
+		bus.write_byte(addr, 0x1) # switch it on
+	elif ledstate == "0":
+		bus.write_byte(addr, 0x0) # switch it on
+	else:
+		numb = 0
